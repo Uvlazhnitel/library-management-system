c3a771b1a542ebd26a2805e2a7eb74c5aa348511
ij23031 <ij23031@edu.lu.lv>
2026-05-25 16:47:12 +0300

    Add User, Reader, Book, and Librarian classes; remove empty .gitkeep file

---
+++ src/main/java/org/example/library/model/Loan.java
@@ -0,0 +1,100 @@
+package org.example.library.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Loan {
+
+    private String loanId;
+    private String bookIsbn;
+    private String readerId;
+    private String borrowDate;
+    private String returnDate;
+    private boolean active;
+
+    public Loan() {
+    }
+
+    public Loan(String bookIsbn, String readerId) {
+        this.loanId = UUID.randomUUID().toString();
+        this.bookIsbn = bookIsbn;
+        this.readerId = readerId;
+        this.borrowDate = LocalDate.now().toString();
+        this.returnDate = null;
+        this.active = true;
+    }
+
+    public Loan(String loanId, String bookIsbn, String readerId, String borrowDate, String returnDate, boolean active) {
+        this.loanId = loanId;
+        this.bookIsbn = bookIsbn;
+        this.readerId = readerId;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+        this.active = active;
+    }
+
+    public void markReturned() {
+        this.returnDate = LocalDate.now().toString();
+        this.active = false;
+    }
+
+    public String getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(String loanId) {
+        this.loanId = loanId;
+    }
+
+    public String getBookIsbn() {
+        return bookIsbn;
+    }
+
+    public void setBookIsbn(String bookIsbn) {
+        this.bookIsbn = bookIsbn;
+    }
+
+    public String getReaderId() {
+        return readerId;
+    }
+
+    public void setReaderId(String readerId) {
+        this.readerId = readerId;
+    }
+
+    public String getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(String borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId='" + loanId + '\'' +
+                ", bookIsbn='" + bookIsbn + '\'' +
+                ", readerId='" + readerId + '\'' +
+                ", borrowDate='" + borrowDate + '\'' +
+                ", returnDate='" + returnDate + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/library/model/Reader.java
@@ -0,0 +1,73 @@
+package org.example.library.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Reader extends User {
+
+    private List<String> borrowedBookIsbns;
+
+    public Reader() {
+        this.borrowedBookIsbns = new ArrayList<>();
+    }
+
+    public Reader(String id, String name, String email) {
+        super(id, name, email);
+        this.borrowedBookIsbns = new ArrayList<>();
+    }
+
+    @Override
+    public String getRole() {
+        return "Reader";
+    }
+
+    public List<String> getBorrowedBookIsbns() {
+        return borrowedBookIsbns;
+    }
+
+    public void setBorrowedBookIsbns(List<String> borrowedBookIsbns) {
+        this.borrowedBookIsbns = borrowedBookIsbns;
+    }
+
+    public void addBorrowedBook(String isbn) {
+        if (!borrowedBookIsbns.contains(isbn)) {
+            borrowedBookIsbns.add(isbn);
+        }
+    }
+
+    public void removeBorrowedBook(String isbn) {
+        borrowedBookIsbns.remove(isbn);
+    }
+
+    public boolean hasBorrowedBook(String isbn) {
+        return borrowedBookIsbns.contains(isbn);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Reader reader = (Reader) o;
+        return Objects.equals(getId(), reader.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "id='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", borrowedBookIsbns=" + borrowedBookIsbns +
+                '}';
+    }
+}
